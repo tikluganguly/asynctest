7b0f8e6ae9e928267a365d8c59525f187047aa98
tiklu <tikluganguly@hotmail.com>
2022-03-27 13:00:12 +0530

    calling two async functions via await

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-use async_std::task::block_on;
+use async_std::task::{block_on, sleep};
+use futures::future::join;
+use std::time;
 
 async fn say_hello(){
     println!("Hello Again");
 }
 
+async fn connect_radis()->String{
+    sleep(time::Duration::from_secs(1)).await;
+    String::from("Radis Connected")
+}
+
+async fn connect_db()->String{
+    sleep(time::Duration::from_secs(1)).await;
+    String::from("Db Connected")
+}
+
+async fn main_executor(){
+    say_hello().await;
+    let (db,radis)= join(connect_db(),connect_radis()).await;
+    println!("{}, {}",db,radis);
+}
+
 fn main() {
-    block_on(say_hello());
+    block_on(main_executor());
 }
